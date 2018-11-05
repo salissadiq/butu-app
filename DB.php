f2351bcc11bd310aa488bc0d894245d807f677b9
salissadiq <salissadiq@gmail.com>
2018-11-05 10:10:50 +0100

    added db config

--- DB.php
+++ DB.php
@@ -1,5 +1,5 @@
 <?php  
-	$db = new mysqli('127.0.0.1', 'bututour_coder', 'OBJECToriented', 'bututour_app');
+	$db = new mysqli('127.0.0.1', 'xxxxxxxx', 'xxxxxxxxx', 'xxxxxxxxx');
 
 	if($db->connect_errno) {
 		//echo $db->connect_error;
